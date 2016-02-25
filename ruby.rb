139097ca966d0013f13543e285b86dcfa9a9666b
Abdirazak Ali <prodef@live.com>
2016-02-25 23:09:40 +0000

    ruby basics

---
+++ ruby.rb
@@ -0,0 +1,10 @@
+puts "Hello, welcome"
+puts "What is your name?"
+
+name = gets.chomp
+
+puts name
+
+print "Notice how print does not make"
+print "A new line, like puts"
+puts "Also looked at the difference between gets and gets.chomp"
