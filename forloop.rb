22e054de7c0cc9a6d8806e5ee80c33325d7164c2
Abdirazak Ali <prodef@live.com>
2016-02-25 23:15:53 +0000

    for loop

---
+++ forloop.rb
@@ -0,0 +1,12 @@
+
+#the variable i within the range will activate the code
+for i in 0..10
+	puts "value of local variable i #{i}"
+end
+
+
+#alternative methd
+
+(0..5).each do |x|
+	puts "value of #{x} now is"
+end
