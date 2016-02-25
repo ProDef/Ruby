e7d556d99849063922dd58aadd5f2223bf75183c
Abdirazak Ali <prodef@live.com>
2016-02-25 23:10:03 +0000

    user input

---
+++ userinput.rb
@@ -0,0 +1,12 @@
+puts "This will show us how to use user input"
+puts "Give me your age and I will show you"
+
+puts "How old are you"
+
+age = gets.to_f
+
+puts age
+
+puts age.class
+
+#to_i would make it an interger. to_f would make it a float value. Seperate classes, fixnum and float.
