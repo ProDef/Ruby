b8d9d9053d6dc020d536d59f7dee87b302f2016b
Abdirazak Ali <prodef@live.com>
2016-02-25 23:12:14 +0000

    if else statements

---
+++ ifelsestatements.rb
@@ -0,0 +1,16 @@
+x = -99
+
+if x > 2
+	puts "The if statement"
+	#Thi shows if the number is anything higher than 2
+elsif x <= 2 and x!= 1
+	puts "The elsif statement is true"
+	#This would show if the first argument is false and the number was 2 or less except 1
+else
+	puts "The else statement no statement is true"
+	#This would show if the first two arguments are not satisfied i.e. false
+end
+
+#and wants both conditions satisfied
+#or would require one of two conditions to be satisfied
+#you can have multiple elsif statements in this method
